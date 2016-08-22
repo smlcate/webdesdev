f7bfc2b480d3d838fec9f1b7ee308f4d2d00cefe
Sam Cate <SamCate@Sams-MacBook-Pro.local>
2016-08-22 14:43:50 -0600

    Added working routes

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,31 @@
 var app = angular.module("mainApp", ['ui.router'])
 app.config(function($stateProvider, $urlRouterProvider) {
 
-    // $urlRouterProvider.otherwise('/');
-    //
-    // $stateProvider
-    //
-    //     // // HOME STATES AND NESTED VIEWS ========================================
-    //     .state('tools', {
-    //         url: '/',
-    //         templateUrl: './index.html'
-    //     })
-    //     //
-    //     // // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-    //     // .state('about', {
-    //     //     // we'll get to this in a bit
-    //     // });
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+
+        // // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: './partials/designer.html'
+        })
+        .state('design', {
+            url: '/design',
+            templateUrl: './partials/designer.html'
+        })
+        .state('login', {
+            url: '/login',
+            templateUrl: './partials/validateUser.html'
+        })
+        .state('signup', {
+            url: '/signup',
+            templateUrl: './partials/validateUser.html'
+        })
+        //
+        // // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        // .state('about', {
+        //     // we'll get to this in a bit
+        // });
 
 });
